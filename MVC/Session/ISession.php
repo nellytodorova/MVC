6efd3581d35e0ea493af160d685e9e2cfcce71e2
nellytodorova <nelly.todorova@yahoo.com>
2013-06-01 18:00:11 +0300

    Add native session functionality.

---
+++ MVC/Session/ISession.php
@@ -0,0 +1,12 @@
+<?php
+namespace MVC\Session;
+
+interface ISession
+{
+    public function getSessionId();
+    public function saveSession();
+    public function destroySession();
+    public function __get($name);
+    public function __set($name, $value);
+}
+?>
